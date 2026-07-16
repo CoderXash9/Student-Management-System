ffeaee981a3dd22f5066a53d0eee82f37413a3c3
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-16 14:55:49 +0530

    modified admin.py

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from .models import Student , Course , Enrollment
 
-# Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+        "phone",
+        "gender",
+    )
+
+    search_fields = (
+        "first_name",
+        "last_name",
+        "email",
+    )
+
+    list_filter = (
+        "gender",
+    )
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "course_name",
+        "course_code",
+        "credits",
+    )
+
+    search_fields = (
+        "course_name",
+        "course_code",
+    )
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "student",
+        "course",
+        "status",
+        "enrollment_date",
+    )
+
+    list_filter = (
+        "status",
+    )
+
+

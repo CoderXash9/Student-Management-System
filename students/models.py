41a066a076bb7990da1827ff5526e519d007bf8e
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-16 14:02:36 +0530

    added more models for course and enrollment

--- students/models.py
+++ students/models.py
@@ -16,3 +16,31 @@ class Student(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=100)
+    course_code = models.CharField(max_length=20, unique=True)
+    description = models.TextField()
+    credits = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.course_name
+
+
+class Enrollment(models.Model):
+    STATUS_CHOICES = [
+        ("Active", "Active"),
+        ("Completed", "Completed"),
+        ("Dropped", "Dropped"),
+    ]
+
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    enrollment_date = models.DateField(auto_now_add=True)
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Active")
+
+    def __str__(self):
+        return f"{self.student} {self.course}"

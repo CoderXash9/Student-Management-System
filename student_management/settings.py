9b03555635044c59e4f754056254613ea376dab8
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-16 12:27:33 +0530

    updated settings.py

--- student_management/settings.py
+++ student_management/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django.contrib.rest_framework" "django.contrib.students",
+    "django.contrib.rest_framework",
+    "django.contrib.students",
 ]
 
 MIDDLEWARE = [

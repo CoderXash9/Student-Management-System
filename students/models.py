d52660ac995a69de47deacad2e981757131bf25a
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-17 18:45:42 +0530

    modified models.py

--- students/models.py
+++ students/models.py
@@ -6,7 +6,7 @@ class Student(models.Model):
 
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    models.EmailField(unique=True)
+    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15)
     date_of_birth = models.DateField()
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES)

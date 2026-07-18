2ea7b49a93f7f14c8525a09647649860f03b42f1
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-18 15:38:46 +0530

    added django filters import in views.py

--- students/views.py
+++ students/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Student, Course, Enrollment
 from .serializers import (
     StudentSerializer,

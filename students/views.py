d5fa00829ed00bc820d3fe6840c1940d818dbf61
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-18 12:07:26 +0530

    created views for serializers

--- students/views.py
+++ students/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Student, Course, Enrollment
+from .serializers import (
+    StudentSerializer,
+    CourseSerializer,
+    EnrollmentSerializer,
+)
 
-# Create your views here.
+
+class StudentViewset(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class CourseViewset(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class EnrollmentViewset(viewsets.ModelViewSet):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer

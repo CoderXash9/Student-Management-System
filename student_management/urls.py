b0c72e96772c8889c3f6e6133b0ed045efeb0287
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-18 16:07:50 +0530

    added JWT in urls.py

--- student_management/urls.py
+++ student_management/urls.py
@@ -4,6 +4,10 @@ from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -14,4 +18,6 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
